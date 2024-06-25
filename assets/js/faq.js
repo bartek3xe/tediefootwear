7b112cf4a33934b3be7d40f1bb4754726bf2ea0d
bartek3xe <bartek3xe@gmail.com>
2024-06-25 13:54:13 +0200

    Prepare styles for wholesale, faq and about pages

---
+++ assets/js/faq.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const questionContainers = document.querySelectorAll('.question-container');
+
+    questionContainers.forEach(container=> {
+        container.addEventListener('click', () => {
+            const paragraph = container.querySelector('p');
+
+            paragraph.classList.toggle('active');
+        })
+    })
+})
