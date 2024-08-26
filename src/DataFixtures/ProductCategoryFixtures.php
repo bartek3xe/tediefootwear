ff8d155160eade812de6270a86528a97331e220e
bartek3xe <bartek3xe@gmail.com>
2024-08-26 15:37:26 +0200

    Add product and category fixtures

--- src/DataFixtures/ProductCategoryFixtures.php
+++ src/DataFixtures/ProductCategoryFixtures.php
@@ -1,18 +1,56 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\DataFixtures;
 
 use App\Entity\ProductCategory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
 class ProductCategoryFixtures extends Fixture
 {
+    public const CATEGORIES = [
+        [
+            'pl' => 'Kapcie damskie',
+            'en' => 'Women slippers',
+        ],
+        [
+            'pl' => 'Kapcie męskie',
+            'en' => 'Men slippers',
+        ],
+        [
+            'pl' => 'Drewniaki damskie',
+            'en' => 'Women clogs',
+        ],
+        [
+            'pl' => 'Drewniaki męskie',
+            'en' => 'Men clogs',
+        ],
+        [
+            'pl' => 'Kapcie dziecięce',
+            'en' => 'Children slippers',
+        ],
+        [
+            'pl' => 'Specjalne oferty',
+            'en' => 'Special offers',
+        ],
+    ];
+
     public function load(ObjectManager $manager): void
     {
-         $product = new ProductCategory();
-         $manager->persist($product);
+        $numberOfCategory = 1;
+        foreach (self::CATEGORIES as $category) {
+            $productCategory = new ProductCategory();
+            $productCategory->setName($category);
+            $manager->persist($productCategory);
 
-         $manager->flush();
+            $this->addReference('category_' . $numberOfCategory, $productCategory);
+
+            $numberOfCategory++;
+        }
+
+        $manager->flush();
     }
 }
