43404302c97c7265aa54dba582b3a15b338298c3
bartek3xe <bartek3xe@gmail.com>
2024-10-08 11:34:24 +0200

    Correct cron.sh file

--- bin/cron.sh
+++ bin/cron.sh
@@ -5,5 +5,5 @@ then
     echo "Command already running."
 else
     echo "Starting command"
-    /opt/alt/php82/usr/bin/php /home/srv51178/domains/tediefootwear.com/public_html/bin/console messenger:consume async --env=local --no-interaction >> /home/srv51178/domains/tediefootwear.com/public_html/var/log/cron_messenger.log 2>&1 &
+    /opt/alt/php82/usr/bin/php /home/srv51178/domains/tediefootwear.com/public_html/bin/console messenger:consume async -vv
 fi
