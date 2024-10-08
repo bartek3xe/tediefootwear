7e377f9ad6db1cf1e678efe7ed90e15861318d38
bartek3xe <bartek3xe@gmail.com>
2024-10-09 01:10:07 +0200

    Correct queries based on new database structure

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -77,9 +77,11 @@ class ProductRepository extends ServiceEntityRepository
     public function findBySearchQuery(string $query, string $locale): array
     {
         return $this->createQueryBuilder('p')
-            ->where('LOWER(JSON_UNQUOTE(JSON_EXTRACT(p.name, :locale))) LIKE LOWER(:query)')
-            ->setParameter('locale', '$.' . $locale)
+            ->innerJoin('p.translations', 't')
+            ->where('LOWER(t.name) LIKE LOWER(:query)')
+            ->andWhere('t.language = :locale')
             ->setParameter('query', '%' . $query . '%')
+            ->setParameter('locale', $locale)
             ->getQuery()
             ->getResult();
     }
